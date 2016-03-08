d306b316cff99154aa0b36994bfd04f296986cfa
ftiar <ftiar@beapi.fr>
2016-03-08 18:12:18 +0100

    Refacto autoloader boilerplate

--- autoload.php
+++ autoload.php
@@ -1,6 +1,5 @@
 <?php
 namespace BEA\PB;
 
-$loader = \BEA\Autoloader::get_instance();
-// register the base directories for the namespace prefix
-$loader->addNamespace( 'BEA\PB', BEA_PB_DIR . 'classes' );
+$loader = \BEA\Autoloader::get_instance(); // @since 3.0
+$loader->addNamespace( 'BEA\PB', BEA_PB_DIR . 'classes' ); // register the base directories for the namespace prefix
