ace91b920a0c20c7da592529117dcad432eeed19
ftiar <ftiar@beapi.fr>
2016-03-08 18:12:41 +0100

    Update Boilerplate version

--- bea-plugin-boilerplate.php
+++ bea-plugin-boilerplate.php
@@ -2,7 +2,7 @@
 /*
  Plugin Name: BEA Plugin Name
  Version: 1.0.0
- Version Boilerplate: 2.1.0
+ Version Boilerplate: 3.0.0
  Plugin URI: http://www.beapi.fr
  Description: Your plugin description
  Author: BE API Technical team
