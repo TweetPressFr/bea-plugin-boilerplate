b955c889e891bed62ddc9d454dd5033f9cfb2813
ftiar <ftiar@beapi.fr>
2016-04-13 17:37:50 +0200

    remove unnecessary comment

--- autoload.php
+++ autoload.php
@@ -1,5 +1,5 @@
 <?php
 namespace BEA\PB;
 
-$loader = \BEA\Autoloader::get_instance(); // @since 3.0
+$loader = \BEA\Autoloader::get_instance();
 $loader->addNamespace( 'BEA\PB', BEA_PB_DIR . 'classes' ); // register the base directories for the namespace prefix
